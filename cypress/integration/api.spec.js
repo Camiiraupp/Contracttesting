f00ca95d94195ab07bdd228a9595581e37d68e1f
camila.raupp <camila.raupp@nkey.com.br>
2022-09-01 15:42:51 -0300

    fix api.spec.js

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -2,16 +2,15 @@ import Ajv from "ajv";
 const ajv = new Ajv({ allErrors: true, verbose: true }); //instanciando
 
 describe("Contrato de API com Cypresss", () => {
-    it("Faz a requisição para API", () => {
-        cy.request("GET", "https://jsonplaceholder.typicode.com/users").then((response) => {
-            cy.fixture("jsonSchema").then((jsonSchema) => {
-                const validate = ajv.compile(jsonSchema);
-                const valid = validate(response.body);
-                if (!valid)
-                    cy.log(validate.errors).then(() => {
-                        throw new Error("Falha do Contrato, ver log acima");
-                    })
-            })
-        })
-    })
-})
+  it("Faz a requisição para API", () => {
+    cy.request("GET", "https://jsonplaceholder.typicode.com/users").then(
+      (response) => {
+        cy.fixture("jsonSchema").then((jsonSchema) => {
+          const validate = ajv.compile(jsonSchema);
+          const valid = validate(response.body);
+          expect(valid).to.equal(true);
+        });
+      }
+    );
+  });
+});
